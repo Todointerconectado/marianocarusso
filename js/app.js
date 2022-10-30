f1a25461b21617b278da5eb54e52662c04dd07a5
Francisco <106126465+Francisco-Carusso@users.noreply.github.com>
2022-10-30 11:23:23 -0300

    V1.0.3 Acordeon 1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Section menu
+// ---- Section menu
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
@@ -10,8 +10,8 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 
-// sacar el menú al seleccionar una sección - bring up the menu when selecting a section
-// Combinación de teclado: alt + 91: []  -  alt + 92: \  -  alt + 94: ^  - alt + 96: ``
+// ---- sacar el menú al seleccionar una sección - bring up the menu when selecting a section
+// ---- Combinación de teclado: alt + 91: []  -  alt + 92: \  -  alt + 94: ^  - alt + 96: ``
 const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
 
 const observer = new IntersectionObserver((entries) => {
@@ -24,7 +24,7 @@ const observer = new IntersectionObserver((entries) => {
             menuLink.classList.add("selected");
         }
     });
-}, {rootMargin: "-40% 0px -60% 0px"});
+}, {rootMargin: "-40% 0px 0% 0px"});
 
 menuLinks.forEach(menuLink => {
     menuLink.addEventListener("click", function() {
@@ -38,17 +38,72 @@ menuLinks.forEach(menuLink => {
     }
 });
 
-// connect the form to whatsapp
-let sendbtn = document.querySelector('#sendbtn');
+// ---- connect the form to whatsapp
+// let sendbtn = document.querySelector('#sendbtn');
 
-sendbtn.addEventListener('click' , enviarMensaje);
+// sendbtn.addEventListener('click',enviarMensaje);
 
-function enviarMensaje() {
-    let inputName = document.querySelector('#name').value
-    let textareaMensaje = document.querySelector('#description').value
+// function enviarMensaje() {
+//     let inputName = document.querySelector('#name').value
+//     let textareaMensaje = document.querySelector('#description').value
 
-    let urlWhatsapp = "https://api.whatsapp.com/send?phone=541154120432&text=Nombre y Apellido: %0A" + inputName + "%0A%0AMensaje: %0A" + textareaMensaje + "%0A";
+//     let urlWhatsapp = "https://api.whatsapp.com/send?phone=541154120432&text=Nombre y Apellido: %0A" + inputName + "%0A%0AMensaje: %0A" + textareaMensaje + "%0A";
 
-    window.open(urlWhatsapp);
-}
+//     window.open(urlWhatsapp);
+// }
 
+
+// ---- Las seccion con despriegue
+const container = document.getElementById('container');
+
+container.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('listTitle')) { // devuelve booleano
+        e.target.parentElement.classList.toggle('scale')
+        e.target.children[1].classList.toggle('rotate')
+    }
+})
+
+
+
+
+
+
+
+
+// function acordeon (){
+//     document.getElementById('acordeonText').classList.toggle ('clickOpen');
+// }
+
+// document.getElementById('boton').onclick = function (){
+//     acordeon();
+// }
+
+
+
+
+// const boton = document.getElementById('boton');
+// const acordeonText = document.querySelectorAll('acordeonText');
+// console.log(acordeonText);
+
+// boton.addEventListener('click',()=>{
+
+//     for(let contador of acordeonText) {
+//         contador.classList.toggle('clickOpen')
+//     }
+
+// });
+
+
+// function acordeon (){
+//     const acordeonText = document.querySelectorAll('acordeonText');
+
+//     boton.addEventListener('click',()=>{
+//         for(let contador=0; contador < acordeonText.length; contador++) {
+//             acordeonText[contador].classList.toggle('clickOpen');
+//         }
+//     });
+// }
+
+// document.getElementById('boton').onclick = function (){
+//     acordeon();
+// }
